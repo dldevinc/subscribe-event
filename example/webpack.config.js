117e8b825d77063a253259d7d3e53df30ae8e102
pix666 <x896321475@gmail.com>
2024-04-26 12:55:14 +0400

    update dependencies

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-    entry: './example/example.js',
+    entry: "./example/example.js",
     output: {
         path: __dirname,
-        filename: 'example.bundle.js'
+        filename: "example.bundle.js"
     }
 };
